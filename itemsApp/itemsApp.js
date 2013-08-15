e31003206bc5302286ebc7489d971dd0c2e97dea
Roman Heinrich <roman.heinrich@gmail.com>
2013-08-15 13:49:56 +0200

    reading limiting on collections

--- itemsApp/itemsApp.js
+++ itemsApp/itemsApp.js
@@ -2,14 +2,27 @@ Items = new Meteor.Collection("items")
 
 
 if (Meteor.isClient){
-  Meteor.subscribe("items");
+  Meteor.autosubscribe(function(){
+    Meteor.subscribe("items", Session.get("category"));
+  })
+
   Template.items.items = function(){
     return Items.find();
   }
+
+  Template.name.events({
+    'keypress input': function(e, t){
+      if( e.keyCode === 13){
+        var input = t.find("input")
+        Session.set("category", input.value)
+        input.value = ""
+      }
+    }
+  })
 }
 
 if (Meteor.isServer){
-  Meteor.publish("items", function(){
-    return Items.find()
+  Meteor.publish("items", function(category){
+    return Items.find({category: category})
   })
 }
